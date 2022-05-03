186c691b23638265e78621a05dd8eba634f35c42
Dayana Tito <Dayanatitochoque@hotmail.com>
2022-05-03 10:36:37 -0300

    Atualizar Classe Model

--- src/main/java/com/generation/discoleta/model/Tema.java
+++ src/main/java/com/generation/discoleta/model/Tema.java
@@ -1,5 +1,51 @@
 package com.generation.discoleta.model;
 
-public class Tema {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	@Size(max = 500)
+	private String descricao;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(max = 100)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
