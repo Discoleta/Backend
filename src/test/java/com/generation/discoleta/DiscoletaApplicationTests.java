5ea4fc2f525e8c8c0a049876d0448e6cc78a548d
Aline <aline.rasche25@gmail.com>
2022-05-03 09:43:42 -0300

    Criar o Projeto Spring

---
+++ src/test/java/com/generation/discoleta/DiscoletaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.discoleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
