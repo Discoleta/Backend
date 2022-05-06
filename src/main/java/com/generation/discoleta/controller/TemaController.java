6d81b7998630f9cc4da4d51b0d1a6a2ed1efec69
insivam <88207216+insivam@users.noreply.github.com>
2022-05-06 10:13:29 -0300

    correcao de bugs

--- src/main/java/com/generation/discoleta/controller/TemaController.java
+++ src/main/java/com/generation/discoleta/controller/TemaController.java
@@ -4,20 +4,21 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import com.generation.discoleta.model.Tema;
-import com.generation.discoleta.repository.TemaRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.discoleta.model.Tema;
+import com.generation.discoleta.repository.TemaRepository;
 
 @RestController
 @RequestMapping("/tema")
@@ -26,18 +27,18 @@ public class TemaController {
 	@Autowired
 	private TemaRepository temaRepository;
 
-	@RequestMapping
+	@GetMapping
 	public ResponseEntity<List<Tema>> getAll() {
 		return ResponseEntity.ok(temaRepository.findAll());
 	}
 
-	@RequestMapping("/{id}") // Método get para listar uma tema pelo id
+	@GetMapping("/{id}") // Método get para listar uma tema pelo id
 	public ResponseEntity<Tema> getById(@PathVariable Long id) {
 		return temaRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.notFound().build());
 	}
 
-	@RequestMapping("/nome/{nome}") // Método get para listar temas pelo gênero
+	@GetMapping("/nome/{nome}") // Método get para listar temas pelo gênero
 	public ResponseEntity<List<Tema>> getBynome(@PathVariable String nome) {
 		return ResponseEntity.ok(temaRepository.findAllByNomeContainingIgnoreCase(nome));
 	}
