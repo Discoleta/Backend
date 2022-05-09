328f228376946407dd5b488e29cb3c731d9ae746
Aline <aline.rasche25@gmail.com>
2022-05-09 11:32:43 -0300

    Classe Model Usuario

---
+++ src/main/java/com/generation/discoleta/model/Usuario.java
@@ -0,0 +1,101 @@
+package com.generation.discoleta.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name = "tb_usuario")
+
+public class Usuario {
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+@NotBlank(message = "o atributo nome é obrigatório")
+@Size(max = 100)
+private String nome;
+
+@NotBlank(message = "o atributo usuario é obrigatório")
+@Size(max = 255)
+private String usuario;
+
+private String foto;
+
+@NotBlank(message = "o atributo senha é obrigatório")
+@Size(min = 8)
+private String senha;
+
+@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
+@JsonIgnoreProperties ("usuario")
+private List <Postagem> postagem;
+
+public List<Postagem> getPostagem() {
+	return postagem;
+}
+
+public void setPostagem(List<Postagem> postagem) {
+	this.postagem = postagem;
+}
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String getUsuario() {
+	return usuario;
+}
+
+public void setUsuario(String usuario) {
+	this.usuario = usuario;
+}
+
+public String getFoto() {
+	return foto;
+}
+
+public void setFoto(String foto) {
+	this.foto = foto;
+}
+
+public String getSenha() {
+	return senha;
+}
+
+public void setSenha(String senha) {
+	this.senha = senha;
+	
+
+}
+
+
+
+
+
+
+
+
+}
